1618bb307f5fe1acac6729b4af6ac1d76c21277d
rjapon <richardjv666@gmail.com>
2026-05-06 08:59:48 -0500

    Guardado

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import controllers.PersonController;
+import models.Person;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Person[] personas = {
+            new Person("Juan", 25),
+            new Person("Ana", 19),
+            new Person("Carlos", 30),
+            new Person("Maria", 22),
+            new Person("Diego", 15),
+        };
+
+        //Imprimir
+        //Ordenar
+        //Imprimir
+
+        for (Person person : personas) {
+            System.out.println(person);
+        }
+
+        PersonController pController = new PersonController();
+        pController.sortPersonByAge(personas);
+
+        System.out.println("\n==== Ordenado ====\n");
+        for (Person person : personas) {
+            System.out.println(person);
+        }
+    }
+
+}
